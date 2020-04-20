fcedbb7d3c7ff3762792db62190296178745cc61
xuzheng.he <442192435@qq.com>
2020-04-20 16:25:55 +0800

    合并区间暴力法

--- 排序/合并区间.js
+++ 排序/合并区间.js
@@ -17,6 +17,8 @@
  * @param {number[][]} intervals
  * @return {number[][]}
  */
+
+// 排序法
 var merge = function(intervals) {
     const sorted = intervals.sort((a, b) => {
         return a[0] - b[0]
@@ -37,4 +39,70 @@ var merge = function(intervals) {
         arr.push(newArr)
     }
     return arr
+};
+
+// 暴力法
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    function conbine (left1, right1) {
+        const left = [...left1]
+        const right = [...right1]
+        if ((left[0] <= right[0]) && (right[0] <= left[1]) && (left[1] <= right[1])) {
+            left[1] = right[1]
+            return {
+                result: left,
+                type: 0
+            }
+        } else if ((right[0] <= left[0]) && (left[0] <= right[1]) && (right[1] <= left[1])) {
+            right[1] = left[1]
+            return {
+                result: right,
+                type: 1
+            }
+        } else if (left[0] <= right[0] && left[1] >= right[1]) {
+            return {
+                result: left,
+                type: 0
+            }
+        } else if (right[0] <= left[0] && right[1] >= left[1]) {
+            return {
+                result: right,
+                type: 1
+            }
+        } else {
+            return false
+        }
+    }
+    const except = []
+    for (let i = 0; i < intervals.length; i++) {
+        if (except.indexOf(i) >= 0) {
+            continue
+        }
+        for (let j = 0; j < intervals.length; j++) {
+            if (except.indexOf(j) >= 0 || i === j) {
+                continue
+            }
+            const result = conbine(intervals[i], intervals[j])
+            if (result) {
+                if (result.type === 0) {
+                    except.push(j)
+                    intervals[i] = result.result
+                } else {
+                    except.push(i)
+                    intervals[j] = result.result
+                    break
+                }
+            }
+        }
+    }
+    const result = []
+    for (let i = 0; i < intervals.length; i++) {
+        if (except.indexOf(i) < 0) {
+            result.push(intervals[i])
+        }
+    }
+    return result
 };
